cf90d8bc0009833bde337a82d95e31886c9ef313
Norby99 <dont.vip@gmail.com>
2021-09-18 17:32:07 +0200

    first setup

---
+++ firstCode.py
@@ -0,0 +1,14 @@
+import numpy as np
+import nnfs
+
+class Layer_Dense:
+
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+
+
+
+    def forward():
+        pass
